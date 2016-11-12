9ea18be05130f1f2bca504001c387503f2dc1abf
Mohan <ms1086607@gmail.com>
2016-11-12 18:31:05 +0530

    Mere Brother Ki Dulhan

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $dataLoader = new DataLoader();
 $data = $dataLoader->load(__DIR__.'/data/tld.json');
 
 $client = new WhoisClient($factory, $data);
-$domainName = 'geteasyDomain.com';
+$domainName = 'geteasyGF4RAHUL.com';
 echo "<pre>".$client->query($domainName);echo "</pre>";
 
 $whoisClient = new WhoisClient($factory, $data);
